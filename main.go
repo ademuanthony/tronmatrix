d17d1ad5ba65e381bddda4d87fabf3ee81b144d6
Ademu Anthony <ademuanthony@gmail.com>
2020-09-27 01:27:57 +0100

    bee conf

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strconv"
 	_ "tronmatrix/routers"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	log.Println("Env $PORT :", os.Getenv("PORT"))
+	if os.Getenv("PORT") != "" {
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			log.Fatal(err)
+			log.Fatal("$PORT must be set")
+		}
+		log.Println("port : ", port)
+		beego.BConfig.Listen.HTTPPort = port
+		beego.BConfig.Listen.HTTPSPort = port
+	}
+	if os.Getenv("BEEGO_ENV") != "" {
+		log.Println("Env $BEEGO_ENV :", os.Getenv("BEEGO_ENV"))
+		beego.BConfig.RunMode = os.Getenv("BEEGO_ENV")
+	}
+
 	beego.Run()
 }
-
