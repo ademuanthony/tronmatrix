210e674f643ae7c4c82160a78252947582be2874
Ademu Anthony <ademuanthony@gmail.com>
2020-10-06 22:12:46 +0100

    shopping countdown

---
+++ static/landing/assets/js/countdown.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    /* =========================================
+        COUNTDOWN 3
+     ========================================= */
+    $('#clock-b').countdown('2020/10/9').on('update.countdown', function(event) {
+      var $this = $(this).html(event.strftime(''
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%D</span> Day%!d</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%H</span> Hr</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%M</span> Min</div>'
+        + '<div class="holder m-2"><span class="h1 font-weight-bold">%S</span> Sec</div>'));
+    });
+
+});
